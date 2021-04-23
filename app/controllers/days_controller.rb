9c6473a04d11a7b065e35e849820fe4bef1a6f6d
Cobbeec <cobbeec@gmail.com>
2021-04-22 20:17:33 -0500

    Adding to days controller

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,2 +1,11 @@
 class DaysController < ApplicationController
+    def index
+        days = Day.all 
+        render json: days  
+       end 
+       
+       def show
+       day = Day.find(params[:id])
+       render json: days 
+       end 
 end 
